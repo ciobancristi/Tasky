1894879b290dc92ec497b4c6201ecbcdfe0eef0e
Cristi Cioban <ciobancristi@gmail.com>
2016-03-27 17:28:43 +0300

    Added: -empty fields validation on login -UserService.cs

---
+++ Tasky.Services/UserService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Tasky.Entities;
+
+namespace Tasky.Services
+{
+
+    public interface IUserService
+    {
+    }
+    public class UserService : BaseService, IUserService
+    {
+        private TaskyDBEntities _dbContext;
+
+        public UserService()
+        {
+            _dbContext = new TaskyDBEntities();
+        }
+
+        #region IDisposable
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (_dbContext != null)
+                {
+                    _dbContext.Dispose();
+                    _dbContext = null;
+                }
+            }
+        }
+        #endregion
+    }
+
+}
--- Tasky/LoginForm.cs
+++ Tasky/LoginForm.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Windows.Forms;
-using Tasky.Entities;
 using Tasky.Services;
 
 namespace Tasky
@@ -8,25 +7,35 @@ namespace Tasky
     public partial class Login : Form
     {
         private IAuthenticationService _authenticationService;
+        private IValidationService _validationService;
         public Login()
         {
             InitializeComponent();
             _authenticationService = new AuthenticationService();
+            _validationService = new ValidationService();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             var userName = UsernameTextbox.Text;
             var password = PasswordTextbox.Text;
-
-            if(_authenticationService.CheckCredentials(userName, password)){
-                var homeForm = new HomeForm();
-                homeForm.Show();
-                this.Hide();
+            string[] fields = {userName, password};
+            if (!_validationService.IsEmptyValidation(fields))
+            {
+                if (_authenticationService.CheckCredentials(userName, password))
+                {
+                    var homeForm = new HomeForm();
+                    homeForm.Show();
+                    Hide();
+                }
+                else
+                {
+                    MessageBox.Show("User/Password incorrect!");
+                }
             }
             else
             {
-                MessageBox.Show("User/Password incorrect");
+                MessageBox.Show("You must provide a user and a password!");
             }
         }
 
