c271a1282a39ec97929ed91075a3341bc42e8b79
Lupu Nicolae <nick4512@gmail.com>
2016-04-14 15:43:00 +0300

    admin register form

--- Tasky/adminNewUserRegistration.Designer.cs
+++ Tasky/adminNewUserRegistration.Designer.cs
@@ -32,9 +32,9 @@
             this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
             this.titleLabel = new System.Windows.Forms.Label();
             this.usernameLabel = new System.Windows.Forms.Label();
-            this.passwordLabel = new System.Windows.Forms.Label();
+            this.emailLabel = new System.Windows.Forms.Label();
             this.usernameTextBox = new System.Windows.Forms.TextBox();
-            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
             this.registerUserButton = new System.Windows.Forms.Button();
             this.roleLabel = new System.Windows.Forms.Label();
             this.roleComboBox = new System.Windows.Forms.ComboBox();
@@ -75,15 +75,15 @@
             this.usernameLabel.TabIndex = 2;
             this.usernameLabel.Text = "Username: ";
             // 
-            // passwordLabel
+            // emailLabel
             // 
-            this.passwordLabel.AutoSize = true;
-            this.passwordLabel.Location = new System.Drawing.Point(67, 122);
-            this.passwordLabel.Name = "passwordLabel";
-            this.passwordLabel.Size = new System.Drawing.Size(59, 13);
-            this.passwordLabel.TabIndex = 3;
-            this.passwordLabel.Text = "Password: ";
-            this.passwordLabel.Click += new System.EventHandler(this.passwordLabel_Click);
+            this.emailLabel.AutoSize = true;
+            this.emailLabel.Location = new System.Drawing.Point(88, 122);
+            this.emailLabel.Name = "emailLabel";
+            this.emailLabel.Size = new System.Drawing.Size(38, 13);
+            this.emailLabel.TabIndex = 3;
+            this.emailLabel.Text = "Email: ";
+            this.emailLabel.Click += new System.EventHandler(this.passwordLabel_Click);
             // 
             // usernameTextBox
             // 
@@ -92,12 +92,12 @@
             this.usernameTextBox.Size = new System.Drawing.Size(158, 20);
             this.usernameTextBox.TabIndex = 4;
             // 
-            // passwordTextBox
+            // emailTextBox
             // 
-            this.passwordTextBox.Location = new System.Drawing.Point(149, 119);
-            this.passwordTextBox.Name = "passwordTextBox";
-            this.passwordTextBox.Size = new System.Drawing.Size(158, 20);
-            this.passwordTextBox.TabIndex = 5;
+            this.emailTextBox.Location = new System.Drawing.Point(149, 119);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(158, 20);
+            this.emailTextBox.TabIndex = 5;
             // 
             // registerUserButton
             // 
@@ -107,6 +107,7 @@
             this.registerUserButton.TabIndex = 6;
             this.registerUserButton.Text = "Register User";
             this.registerUserButton.UseVisualStyleBackColor = true;
+            this.registerUserButton.Click += new System.EventHandler(this.registerUserButton_Click);
             // 
             // roleLabel
             // 
@@ -125,7 +126,7 @@
             this.roleComboBox.Size = new System.Drawing.Size(158, 21);
             this.roleComboBox.TabIndex = 8;
             // 
-            // adminNewUserRegistration
+            // AdminNewUserRegistration
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -133,15 +134,15 @@
             this.Controls.Add(this.roleComboBox);
             this.Controls.Add(this.roleLabel);
             this.Controls.Add(this.registerUserButton);
-            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.emailTextBox);
             this.Controls.Add(this.usernameTextBox);
-            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.emailLabel);
             this.Controls.Add(this.usernameLabel);
             this.Controls.Add(this.titleLabel);
             this.Controls.Add(this.statusStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Margin = new System.Windows.Forms.Padding(2);
-            this.Name = "adminNewUserRegistration";
+            this.Name = "AdminNewUserRegistration";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "adminNewUserRegistration";
             this.statusStrip1.ResumeLayout(false);
@@ -157,9 +158,9 @@
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
         private System.Windows.Forms.Label titleLabel;
         private System.Windows.Forms.Label usernameLabel;
-        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.Label emailLabel;
         private System.Windows.Forms.TextBox usernameTextBox;
-        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.TextBox emailTextBox;
         private System.Windows.Forms.Button registerUserButton;
         private System.Windows.Forms.Label roleLabel;
         private System.Windows.Forms.ComboBox roleComboBox;
--- Tasky/adminNewUserRegistration.cs
+++ Tasky/adminNewUserRegistration.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Tasky.Entities;
 
 namespace Tasky
 {
@@ -19,7 +20,28 @@ namespace Tasky
 
         private void passwordLabel_Click(object sender, EventArgs e)
         {
+    }
 
+        private void registerUserButton_Click(object sender, EventArgs e)
+        {
+
+            UserDetail _userDetails = new UserDetail();
+            User _user = new User();
+            Position _userPosition = new Position();
+            _user.UserId = Guid.NewGuid();
+            _user.Name = usernameTextBox.Text;
+            _userDetails.Email = emailTextBox.Text;
+            _user.Password=GeneratePassword();
+
+        }
+
+        private static string GeneratePassword()
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            var random = new Random();
+            var length = 10;
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
         }
     }
 }
